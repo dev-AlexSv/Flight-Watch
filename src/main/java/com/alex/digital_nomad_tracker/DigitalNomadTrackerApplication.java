789f62b6f5fa2514727474a7af70af9a7623505b
devAlex <janalexandersilvestree@gmail.com>
2026-08-01 14:59:18 +0800

    Created The Project Structure

---
+++ src/main/java/com/alex/digital_nomad_tracker/DigitalNomadTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.alex.digital_nomad_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalNomadTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalNomadTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/digital_nomad_tracker/DigitalNomadTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.digital_nomad_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalNomadTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
